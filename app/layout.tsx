64a20b82fe246f22545acf832bdc67aad6f2296a
Trần  Giáp <giap.tran@mona.host>
2025-04-11 10:44:56 +0700

    update somethings

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sửa Khóa Thanh Hóa - Thợ sửa khóa tại nhà 24h",
-  description:
-    "Dịch vụ sửa khóa tại nhà 24h tại Thanh Hóa. Sửa khóa cửa, khóa xe máy, khóa ô tô, chìa khóa thông minh.",
+  description: "Dịch vụ sửa khóa tại nhà 24h tại Thanh Hóa...",
+  icons: {
+    // Hoặc Cách 2: Dùng mảng các icon với nhiều kích thước (PNG/SVG)
+    icon: [
+      { url: "/images/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+  },
 };
 
 export default function RootLayout({
