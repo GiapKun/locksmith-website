bbc759c070f54d0ddb42509f145f63816ff35a6b
Trần  Giáp <giap.tran@mona.host>
2025-04-11 12:17:20 +0700

    remove unused code

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { Menu, X, Phone, PhoneCall } from "lucide-react";
+import { Menu, X, PhoneCall } from "lucide-react";
 import { useState, useEffect } from "react";
 
 export default function Navigation() {
