b441da9568ce89e9c4d6708237fe6dd3dc4d3e19
Trần  Giáp <giap.tran@mona.host>
2025-04-30 18:26:32 +0700

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://suakhoathanhhoa.com",
-  generateRobotsTxt: true, // (optional)
-  // Other options:
-  // changefreq: 'daily',
-  // priority: 0.7,
-  // sitemapSize: 5000,
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: "./public", // Quan trọng khi dùng Vercel
 };
