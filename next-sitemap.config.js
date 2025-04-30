f96f29e836498836e2997f8c406af654857a6ddc
Trần  Giáp <giap.tran@mona.host>
2025-04-30 17:26:59 +0700

    add config sitemap and robots.txt file

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://suakhoathanhhoa.com",
+  generateRobotsTxt: true, // (optional)
+  // Other options:
+  // changefreq: 'daily',
+  // priority: 0.7,
+  // sitemapSize: 5000,
+};
